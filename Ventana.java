c54bfd9cace31e7f008e9e1faba82509a1bdd006
StevenCarrilloLoor <stevencarrilloloor@gmail.com>
2023-10-03 14:37:40 -0500

    Version 0.5

--- Ventana.java
+++ Ventana.java
@@ -8,22 +8,40 @@ public class Ventana {
     private JButton sacarElementoButton;
     private JButton elementoTopeButton;
     private JButton listaDeEElemnetosButton;
+    private JTextArea PilaLista;
+    private JPanel Principal;
+    private JLabel Ingreso;
     private Pila coleccion = new Pila();
 
     //-----------------------------------------------
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Ventana");
+        frame.setContentPane(new Ventana().Principal);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 
     public Ventana() {
 
         ponerElemntoButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                coleccion.insertar(Integer.parseInt(insertarElemnentoDeLaTextField.getText()));
+                JOptionPane.showMessageDialog(null,"Insetado correctamente");
             }
         });
 
         sacarElementoButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                try {
+                    JOptionPane.showMessageDialog(null,"Eliminadooo: "+coleccion.eliminar());
+                } catch (Exception ex){
+                    JOptionPane.showMessageDialog(null,ex.getMessage());
+
+                }
 
             }
         });
@@ -31,6 +49,12 @@ public class Ventana {
         elementoTopeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                try {
+                    JOptionPane.showMessageDialog(null,"CIMA: "+coleccion.consultarTope());
+                } catch (Exception ex){
+                    JOptionPane.showMessageDialog(null,ex.getMessage());
+
+                }
 
             }
         });
@@ -38,9 +62,12 @@ public class Ventana {
         listaDeEElemnetosButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                listaDeEElemnetosButton.setText(coleccion.toString());
 
             }
         });
+
     }
+
 }
 
